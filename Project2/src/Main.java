bb296eaa501d5bb45aa9911d03e2fd60d7d82d06
Dimitri Naumov <nd76@gmx.de>
2023-08-06 18:04:44 +0200

    feat: add Hallo

--- Project2/src/Main.java
+++ Project2/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Всем Привет");
     System.out.println("Всем bye");
+    System.out.println("Всем Hallo");
   }
 
 }
